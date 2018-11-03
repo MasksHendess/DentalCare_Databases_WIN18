c4b046084c4c59d69c0d8eb1a78f4821e8250720
Max <nekohigan@gmail.com>
2018-11-03 20:37:19 +0100

    Add project files.

---
+++ DataLagringLAB03-DentalCare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataLagringLAB03_DentalCare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
